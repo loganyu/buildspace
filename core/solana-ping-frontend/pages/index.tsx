f95c98abd7a291bce85a08b288aec663ef579c45
Logan Yu <yu.logan@gmail.com>
2023-05-07 22:56:34 +0000

    frontend ping and client

---
+++ core/solana-ping-frontend/pages/index.tsx
@@ -0,0 +1,29 @@
+import { NextPage } from 'next'
+import styles from '../styles/Home.module.css'
+import { AppBar } from '../components/AppBar'
+import Head from 'next/head'
+import { PingButton } from '../components/PingButton'
+import { BalanceDisplay } from '../components/BalanceDisplay'
+import { SendSolForm } from '../components/SendSolForm'
+
+const Home: NextPage = (props) => {
+
+  return (
+    <div className={styles.App}>
+      <Head>
+        <title>Wallet-Adapter Example</title>
+        <meta
+          name="description"
+          content="Wallet-Adapter Example"
+        />
+      </Head>
+      <AppBar />
+      <div className={styles.AppBody}>
+        <BalanceDisplay />
+        <SendSolForm />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
