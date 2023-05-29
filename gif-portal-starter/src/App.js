4c4ac3f0de197339334f582fb420fb9a388ed4ec
Logan Yu <yu.logan@gmail.com>
2023-05-29 12:21:44 -0400

    add portal work

--- gif-portal-starter/src/App.js
+++ gif-portal-starter/src/App.js
@@ -6,6 +6,31 @@ const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+  /*
+   * This function holds the logic for deciding if a Phantom Wallet is
+   * connected or not
+   */
+  const checkIfWalletIsConnected = async () => {
+  // We're using optional chaining (question mark) to check if the object is null
+    if (window?.solana?.isPhantom) {
+      console.log('Phantom wallet found!');
+    } else {
+      alert('Solana object not found! Get a Phantom Wallet 👻');
+    }
+  };
+
+  /*
+   * When our component first mounts, let's check to see if we have a connected
+   * Phantom Wallet
+   */
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected();
+    };
+    window.addEventListener('load', onLoad);
+    return () => window.removeEventListener('load', onLoad);
+  }, []);
+
   return (
     <div className="App">
       <div className="container">
